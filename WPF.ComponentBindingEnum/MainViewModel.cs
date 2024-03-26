45153727c37bdf878c3dd5e56a6028f3386ca805
凉风 <1007172936@qq.com>
2024-03-26 22:35:48 +0800

    两种方式实现绑定Enum

---
+++ WPF.ComponentBindingEnum/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WPF.ComponentBindingEnum
+{
+    partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        PlotModelType modelType;
+    }
+}
