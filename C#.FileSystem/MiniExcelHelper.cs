b2537a1b6f6b256317d775d0ae301852dc0ec15f
凉风 <48055872+CoolBreeze37@users.noreply.github.com>
2024-07-19 17:16:42 +0800

    1

---
+++ C#.FileSystem/MiniExcelHelper.cs
@@ -0,0 +1,32 @@
+﻿using MiniExcelLibs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.FileSystem
+{
+    public static class MiniExcelHelper
+    {
+        public static void TestExportClassToCsv()
+        {
+            var config = new MiniExcelLibs.Csv.CsvConfiguration()
+            {
+                Seperator = ','
+            };
+            var path = "demo.csv";
+            var values = new[] { new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true },
+            new UserAccount() {  Name="1",Age=1, BoD = DateTime.Now, ID = Guid.NewGuid(), Points = 1, VIP = true }};
+            MiniExcel.SaveAs(path,values,overwriteFile:true, configuration: config);
+
+        }
+    }
+}
---
+++ C#.FileSystem/UserAccent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.FileSystem
+{
+    public class UserAccount
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public DateTime BoD { get; set; }
+        public int Age { get; set; }
+        public bool VIP { get; set; }
+        public decimal Points { get; set; }
+    }
+}
