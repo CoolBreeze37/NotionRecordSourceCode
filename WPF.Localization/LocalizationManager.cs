73081b0c5398b0a073cc1b9c015d8ecb1b8d7bc4
凉风 <1007172936@qq.com>
2024-07-23 02:43:15 +0800

    1
    1

---
+++ WPF.Localization/LocalizationManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Localization
+{
+    public static class LocalizationManager
+    {
+        private static ResourceManager _resourceManager = new ResourceManager("WPF.Localization.Resources.Lang", typeof(LocalizationManager).Assembly);
+
+        public static string GetString(string key)
+        {
+            return _resourceManager.GetString(key);
+        }
+
+        public static void ChangeLanguage(string cultureCode)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureCode);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureCode);
+        }
+    }
+}
---
+++ WPF.Localization/LocalizedStrings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Localization
+{
+    public class LocalizedStrings : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static LocalizedStrings _instance = new LocalizedStrings();
+
+        public static LocalizedStrings Instance => _instance;
+
+        public string this[string key] => LocalizationManager.GetString(key);
+
+        public void Refresh()
+        {
+            OnPropertyChanged("");
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
